1d86027a89d4d642fa285a03e5e34ef4fddb008c
mailtoak82 <mailtoak82@gmail.com>
2014-07-21 13:32:16 +0530

    ng-repeat

--- app.js
+++ app.js
@@ -2,17 +2,27 @@
 var app = angular.module('store',[]);
 
 app.controller ('StoreController',function(){
-this.product = gem;
+this.products = gems;
 
 });
 
-var gem = {
+var gems = [ 
 
+{
 	name: 'Dodecahedron',
 	price: 2.95,
 	description: 'This is a super gem!',
 	canPurchase: true,
 	soldOut: false
+},
+
+{
+	name: 'Pentagonal Gem',
+	price: 5.95,
+	description: 'This is a super duper gem!',
+	canPurchase: false,
+	soldOut: false
 }
+];
 
 }) ();
