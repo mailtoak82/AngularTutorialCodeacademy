3068779d984b91a8feb73fbcd4dcb23a6c7bffe0
mailtoak82 <mailtoak82@gmail.com>
2014-07-21 11:56:20 +0530

    Added Controller and Gem Variable

---
+++ app.js
@@ -0,0 +1,16 @@
+(function () {
+var app = angular.module('store',[]);
+
+app.controller ('storeController',function(){
+
+
+});
+
+var gem = {
+
+	name: 'Dodecahedron',
+	price: 2.95,
+	description: 'This is a super gem!'
+}
+
+}) ();
